5d4beb3de6929e213a0cb82d70df6ac7828c867b
Domek1232 <83282741+Domek1232@users.noreply.github.com>
2022-12-06 20:11:45 +0100

    HTML , JS v3 teljes kész verzió
    a HTML illetve a JavaScript v3 teljes kész verziója

--- js/script.js
+++ js/script.js
@@ -5,10 +5,15 @@ const score = document.getElementById("score");
 function jump() {
   dino.classList.add("jump-animation");
   setTimeout(() =>
-    dino.classList.remove("jump-animation"), 500);
+    dino.classList.remove("jump-animation"), 650);
 }
 
 document.addEventListener('keypress', (event) => {
+  if (!dino.classList.contains('jump-animation')) {
+    jump();
+  }
+})
+document.addEventListener("touchstart", (event) => {
   if (!dino.classList.contains('jump-animation')) {
     jump();
   }
@@ -19,17 +24,16 @@ setInterval(() => {
     .getPropertyValue('top'));
   const rockLeft = parseInt(window.getComputedStyle(rock)
     .getPropertyValue('left'));
-  score.innerText++;
+  setTimeout(() => {  score.innerText++; }, 50);
 
-  if (rockLeft < 0) {
+  if (rockLeft < -25) {
     rock.style.display = 'none';
   } else {
     rock.style.display = ''
   }
 
   if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
-    alert("You got a score of: " + score.innerText +
-      "\n\nPlay again?");
+    alert(score.innerText + " pontod lett... 🥳" + "\nújrapróbálod?");
     location.reload();
   }
 }, 50);
