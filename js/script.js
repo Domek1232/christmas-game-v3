544057718be49e80f7db5c5875a255d590168e77
Domek1232 <83282741+Domek1232@users.noreply.github.com>
2022-12-07 13:20:33 +0100

    Christmas-game v3 teljes verzió

--- js/script.js
+++ js/script.js
@@ -18,22 +18,32 @@ document.addEventListener("touchstart", (event) => {
     jump();
   }
 })
+document.addEventListener("mousedown", (event) => {
+  if (!dino.classList.contains('jump-animation')) {
+    jump();
+  }
+})
 
 setInterval(() => {
-  const dinoTop = parseInt(window.getComputedStyle(dino)
-    .getPropertyValue('top'));
-  const rockLeft = parseInt(window.getComputedStyle(rock)
-    .getPropertyValue('left'));
-  setTimeout(() => {  score.innerText++; }, 50);
+  const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+  const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
+  score.innerText++;
 
-  if (rockLeft < -25) {
+  if (rockLeft < -30) {
     rock.style.display = 'none';
-  } else {
+  }
+  else {
     rock.style.display = ''
   }
 
-  if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
-    alert(score.innerText + " pontod lett... 🥳" + "\nújrapróbálod?");
+
+  if (rockLeft < 68 && rockLeft > 0 && dinoTop > 130) {
+    if (score.innerText > 800) {
+      alert(score.innerText + " pontod lett... 🥳 \nnyertél, ügyi vagy :) \nkódod: MAJON1");
+    }
+    else {
+      alert(score.innerText + " pontod lett... 🥳" + "\nújrapróbálod?");
+    }
     location.reload();
   }
 }, 50);
